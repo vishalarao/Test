4377d8b51c32ae4b79f3ad8c6ce72ec789690c76
Rao <vishal.a.rao@accenture.com>
2019-06-25 15:08:23 +0530

    firstapp.py

---
+++ firstapp.py
@@ -0,0 +1,16 @@
+from flask import Flask
+import sys
+import optparse
+import time
+
+app = Flask(__name__)
+start = int(round(time.time()))
+
+@app.route("/")
+def hello_world():
+
+    return "Hello World"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(args.port), debug=False)
+    #app.run(port=5000, debug=False)
