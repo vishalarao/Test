086557345018803af5bbc54139fb599a338e4e8a
vishalarao <52199571+vishalarao@users.noreply.github.com>
2019-06-28 00:18:53 +0530

    Update firstapp.py

--- firstapp.py
+++ firstapp.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def hello_world():
     '''Simple function'''
     return "Hello World"
-
+print "Hello"
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5999, debug=False)
     #app.run(port=5000, debug=False)
