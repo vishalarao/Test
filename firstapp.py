0159b6c7846cfb4a02ddcb652a73a7add145ff4a
vishalarao <52199571+vishalarao@users.noreply.github.com>
2019-07-15 23:05:22 +0530

    Update firstapp.py

--- firstapp.py
+++ firstapp.py
@@ -7,5 +7,5 @@ def hello_world():
     '''Simple function'''
     return "Hello World"
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5999, debug=False)
+    app.run(host='0.0.0.0', port=5151, debug=False)
     #app.run(port=5000, debug=False)
